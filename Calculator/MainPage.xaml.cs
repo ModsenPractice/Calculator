033a7f7748b73e3125a3da6a6ef07557813d7566
Plaksa Anastasia <plaksanasta13@gmail.com>
2024-06-13 23:18:53 +0300

    Add interface for functions and variables

--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -6,17 +6,23 @@ namespace Calculator
 {
     public partial class MainPage : ContentPage
     {
-        int currentState = 1;
-        string operatorMath;
         string expression;
         string calculationResult;
-
+        public List<string> Functions { get; set; }
+        public List<string> Variables { get; set; }
         public MainPage()
         {
             InitializeComponent();
             OnClear(this, null);
+            Functions = new List<string> { "f(x,y)=sqrt(2)+2-4+8", "f1(x,y)=x^(2)+15-7", "f2(x,y)=(pi*2/4)*180"};
+            //Variables = new List<string> { "pi=3.14", "x=5", "y=8" };
+            variablesList.ItemsSource = new List<string> { "pi=3.14", "x=5", "y=8" };
+            BindingContext = this;
         }
-
+        private void variablesList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            selected.Text = $"Выбрано: {e.SelectedItem}";
+        }
         private void OnClear(object sender, EventArgs e)
         {
             this.result.Text = "0"; 
@@ -28,76 +34,50 @@ namespace Calculator
 
         private void OnDelete(object sender, EventArgs e)
         {
-            this.result.Text = this.result.Text.Substring(0, this.result.Text.Length -1);
+            if (this.result.Text != string.Empty)
+                this.result.Text = this.result.Text.Substring(0, this.result.Text.Length - 1);
+            else
+                OnClear(this, null);
+
         }
 
-        private void OnSquareRoot(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-
-            string btnPressed = ((char)0x221A).ToString(); 
-            if (this.result.Text == "0" || currentState < 0)
-            {
-                this.result.Text = string.Empty;
-                if (currentState < 0)
-                    currentState *= -1;
-            }
-
-            this.result.Text += btnPressed;
-        }
-
-        private void OnDegree(object sender, EventArgs e)
+        private void OnSpecificSymbol(object sender, EventArgs e)
         {
             Button button = (Button)sender;
             string btnPressed = button.Text;
 
-            if (this.result.Text == "0" || currentState < 0)
-            {
+            if (this.result.Text == "0")
                 this.result.Text = string.Empty;
-                if (currentState < 0)
-                    currentState *= -1;
+             
+            switch(btnPressed)
+            {
+                case "sqrt":
+                    btnPressed = "sqrt(";
+                    break;
+                case "^2":
+                    btnPressed = "^(2)";
+                    break;
+                case "^n":
+                    btnPressed = "^(";
+                    break;
             }
-
-            if(this.result.Text == "^2")
-                btnPressed = "^(2)";
-            if (this.result.Text == "^n")
-                btnPressed = "^(";
 
             this.result.Text += btnPressed;
 
         }
 
-        private void OnToken(object sender, EventArgs e)
+        private void OnSymbol(object sender, EventArgs e)
         {
             Button button = (Button)sender;
             string btnPressed = button.Text;
 
-            if (this.result.Text == "0" || currentState < 0)
-            {
+            if (this.result.Text == "0")
                 this.result.Text = string.Empty;
-                if (currentState < 0)
-                    currentState *= -1;
-            }
 
             this.result.Text += btnPressed;
         }
 
-        private void OnNumber(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            string btnPressed = button.Text;
-
-            if(this.result.Text == "0" || currentState < 0 )
-            {
-                this.result.Text = string.Empty;
-                if (currentState < 0)
-                    currentState *= -1;
-            }
-
-            this.result.Text += btnPressed;
-        }
-
-        private void OnEqual(object sender, EventArgs e)
+        private void OnCalculate(object sender, EventArgs e)
         {
             Button button = (Button)sender;
             expression = this.result.Text;
@@ -105,6 +85,11 @@ namespace Calculator
         }
 
         async void OnVariable(object sender, EventArgs e)
+        {
+
+        }
+
+        async void OnFunction(object sender, EventArgs e)
         {
 
         }
