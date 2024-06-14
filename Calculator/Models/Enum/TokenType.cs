f75d0d0dc000700850370fd80b56d1712c8bb5f4
Kirill Meleshko <meleshkokirill004@gmail.com>
2024-06-14 13:46:07 +0300

    Added two new interfaces for parser and dataservice. Removed old ITokensParser interface. Added extra methods to Validator manager. Changed some types in TokenType

--- Calculator/Models/Enum/TokenType.cs
+++ Calculator/Models/Enum/TokenType.cs
@@ -8,13 +8,12 @@ namespace Calculator.Models.Enum
 {
     public enum TokenType
     {
-        Plus,
-        Minus,
-        Multiplication,
-        Division,
-        Power,
+        Operations,
         Operand,
         OpenPar,
-        ClosePar
+        ClosePar,
+        Function,
+        Unary,
+
     }
 }
---
+++ Calculator/Services/Interfaces/IDataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Services.Interfaces
+{
+    public interface IDataService
+    {
+        Task<IEnumerable<T>> GetAsync<T>();
+        Task AddAsync<T>(string expression);
+        Task DeleteAsync<T>(string name);
+    }
+}
