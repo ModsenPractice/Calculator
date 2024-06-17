56f6f96507857084a05f4e87563618bf3d13972d
Kirill Meleshko <meleshkokirill004@gmail.com>
2024-06-17 12:08:52 +0300

    Fixed substitution tests

--- Calculator.Tests/Services/FunctionSubstitutionTest.cs
+++ Calculator.Tests/Services/FunctionSubstitutionTest.cs
@@ -3,73 +3,81 @@ using Calculator.Services;
 using Calculator.Services.Data.Interfaces;
 using Calculator.Services.Interfaces;
 
-[TestClass]
-public class FunctionSubstitutionTest
+namespace Calculator.Tests.Services
 {
-    private ISubstitutionService _substitutionService; 
-
-    public FunctionSubstitutionTest()
+    [TestClass]
+    public class FunctionSubstitutionTest
     {
-        _substitutionService = new FunctionSubstitutionService(new TestRepository());
-    }
+        private ISubstitutionService _substitutionService;
 
-    [TestMethod]
-    public async Task SimpleFunction(){
-        string result = await _substitutionService.ReplaceAsync("f(2)");
-        string expectedResult = "(2+1)"; 
-        Assert.AreEqual(expectedResult, result); 
-    }
-
-    [TestMethod]
-    public async Task ManyParams(){
-        string result = await _substitutionService.ReplaceAsync("ff(2,4)");
-        string expectedResult = "(2+1+4+2*2)"; 
-        Assert.AreEqual(expectedResult, result); 
-    }
-
-    [TestMethod]
-    public async Task Manyfunctions(){
-        string result = await _substitutionService.ReplaceAsync("ff(2,4)+f(8)");
-        string expectedResult = "(2+1+4+2*2)+(8+1)"; 
-        Assert.AreEqual(expectedResult, result); 
-    }
-
-    [TestMethod]
-    public async Task FuntionInFuntion(){
-        string result = await _substitutionService.ReplaceAsync("ff(f(8),4)");
-        string expectedResult = "((8+1)+1+4+2*(8+1))"; 
-        Assert.AreEqual(expectedResult, result); 
-    }
-
-    [TestMethod]
-    public async Task ParamContainsNameOfOther(){
-        string result = await _substitutionService.ReplaceAsync("ff(x,ax)");
-        string expectedResult = "(x+1+ax+2*x)"; 
-        Assert.AreEqual(expectedResult, result); 
-    }
-
-    [TestMethod]
-    public async Task RepeatedFuntions(){
-        string result = await _substitutionService.ReplaceAsync("f(2)+f(4)");
-        string expectedResult = "(2+1)+(4+1)"; 
-        Assert.AreEqual(expectedResult, result); 
-    }
-
-    class TestRepository : IFunctionRepository
-    {
-        public Task AddFunctionAsync(Function function)
+        public FunctionSubstitutionTest()
         {
-            throw new NotImplementedException();
+            _substitutionService = new FunctionSubstitutionService(new TestRepository());
         }
 
-        public Task DeleteFunctionAsync(string name)
+        [TestMethod]
+        public async Task SimpleFunction()
         {
-            throw new NotImplementedException();
+            string result = await _substitutionService.ReplaceAsync("f(2)");
+            string expectedResult = "((2)+1)";
+            Assert.AreEqual(expectedResult, result);
         }
 
-        public async Task<IEnumerable<Function>> GetFunctionsAsync()
+        [TestMethod]
+        public async Task ManyParams()
         {
-            return new List<Function>(){
+            string result = await _substitutionService.ReplaceAsync("ff(2,4)");
+            string expectedResult = "((2)+1+(4)+2*(2))";
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public async Task Manyfunctions()
+        {
+            string result = await _substitutionService.ReplaceAsync("ff(2,4)+f(8)");
+            string expectedResult = "((2)+1+(4)+2*(2))+((8)+1)";
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public async Task FuntionInFuntion()
+        {
+            string result = await _substitutionService.ReplaceAsync("ff(f(8),4)");
+            string expectedResult = "((((8)+1))+1+(4)+2*(((8)+1)))";
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public async Task ParamContainsNameOfOther()
+        {
+            string result = await _substitutionService.ReplaceAsync("ff(x,ax)");
+            string expectedResult = "((x)+1+(ax)+2*(x))";
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public async Task RepeatedFuntions()
+        {
+            string result = await _substitutionService.ReplaceAsync("f(2)+f(4)");
+            string expectedResult = "((2)+1)+((4)+1)";
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        class TestRepository : IFunctionRepository
+        {
+            public Task AddFunctionAsync(Function function)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task DeleteFunctionAsync(string name)
+            {
+                throw new NotImplementedException();
+            }
+
+            public async Task<IEnumerable<Function>> GetFunctionsAsync()
+            {
+                return new List<Function>(){
                 new Function(){
                     Name = "f", Expression = "x+1", Params = new List<string>(){"x"}
                 },
@@ -77,6 +85,7 @@ public class FunctionSubstitutionTest
                     Name = "ff", Expression = "x+1+y+2*x", Params = new List<string>(){"x", "y"}
                 }
             };
+            }
         }
     }
 }
